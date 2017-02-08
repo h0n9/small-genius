cbb2de3206c178ad97ed828368657467636563db
monsieur.ahn <monsieur.ahn@h0n9-2.local>
2017-02-08 15:09:34 +0900

    Initial Commit

---
+++ Chet.forClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chet.forClient
+//
+//  Created by h0n9 on 2017. 2. 8..
+//  Copyright © 2017년 small genius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
